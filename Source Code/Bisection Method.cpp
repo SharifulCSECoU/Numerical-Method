f0ddc3d360c423c5dd2cd683548560f0e398b425
SharifulCSECoU <30752741+SharifulCSECoU@users.noreply.github.com>
2018-09-29 18:34:54 +0600

    Create Bisection Method.cpp

---
+++ Source Code/Bisection Method.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define accuracy 0.0001
+using namespace std;
+
+int degree;
+double m,n,x,z,sum;
+int arr[100];
+
+double fun(double x)
+{
+    sum=0;
+    int i; z=degree;
+    for(i=0;i<=degree;i++)
+    {
+        sum+=arr[i]*pow(x,z);
+        z--;
+    }
+    return sum;
+}
+
+int main()
+{
+    int i;
+
+    cout<<"Enter the highest degree of the equation: "<<endl;
+    cin>>degree;
+
+    cout<<"Enter the co-efficients: "<<endl;
+    for(i=0;i<=degree;i++)
+        cin>>arr[i];
+
+    cout<<"Enter two real numbers assuming the root: "<<endl;
+    cin>>m>>n;
+
+    while(fun(m)*fun(n)>=0)
+    {
+        cout<<"Enter two real numbers assuming the root again: "<<endl;
+        cin>>m>>n;
+    }
+
+    while(abs(n-m)>accuracy)
+    {
+        x=(m+n)/2;
+        if(fun(x)==0) break;
+        else if(fun(m)*fun(x)<0) n=x;
+        else m=x;
+    }
+
+    cout<<"The root of the equation is: "<<x<<endl;
+    return 0;
+}
